5394f87e911493439db0b09c97b8c95ff42dbdc0
JSarasPaySys <jsaras@paymentsystemscorp.com>
2014-10-14 19:23:20 -0700

    everything done but the animations and images

---
+++ components/scripts/script.js
@@ -0,0 +1,12 @@
+$(function() {
+	var wheight = $(window).height(); // get window height
+
+	$(".fullheight").css('height', wheight);
+
+	$(window).resize(function() {
+		var wheight = $(window).height(); // get window height
+
+		$(".fullheight").css('height', wheight);
+
+	});
+});
